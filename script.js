8a83225ee927441388fc076376abf823871a3f3e
gaboramirez <ignacio98ramirez@gmail.com>
2022-05-21 16:55:59 -0300

    cambios en el script

--- script.js
+++ script.js
@@ -13,9 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
         catch{
             console.log('error')
         }
-        finally{
-            console.log('finally')
-        }
     }
     fetchDato()
 })
@@ -25,8 +22,8 @@ const pintarDato = (dato) => {
     Object.values(dato).forEach(productos => {
         const div = document.createElement('div');
         div.className = 'card';
-        const Img = document.createElement('Img');
-        Img.setAttribute('src', productos.url);
+        const img = document.createElement('img');
+        img.setAttribute('src', productos.url);
         const title = document.createElement('h3');
         title.innerHTML = productos.title;
         const button = document.createElement('button');
@@ -35,10 +32,43 @@ const pintarDato = (dato) => {
 
 
 
-        div.appendChild(Img)
+        div.appendChild(img)
         div.appendChild(title)
         div.appendChild(button)
 
         contenedorProductos.appendChild(div)
     });
+}
+
+document.addEventListener('click', (e) =>{
+    if(e.target.matches('.btn')){
+        card = e.target.parentElement
+
+        const productoCard = {
+            img: card.querySelector('img').src,
+            title: card.querySelector('h3').textContent,
+            stock: 1
+        }
+
+        asideProductos.push(productoCard)
+
+        agregarProductos(productoCard)
+
+        localStorage.setItem('carrito', JSON.stringify(asideProductos))
+    }
+
+    if(e.target.matches('.icon') || e.target.matches('.icon *')){
+        aside.classList.toggle('active')
+    }
+})
+
+const agregarProductos = (productoCard) => {
+    const div = document.createElement('div');
+    div.className = 'producto';
+    const img = document.createElement('img');
+    img.setAttribute('src', productoCard.img);
+
+    div.appendChild(img)
+
+    aside.appendChild(div)
 }
