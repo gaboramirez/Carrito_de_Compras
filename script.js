3d8169dee8bd8aaf44fd4b3391bfd8d4f5eaaa47
gaboramirez <ignacio98ramirez@gmail.com>
2022-05-21 15:10:35 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const contenedorProductos = document.querySelector('.contenedor-productos');
+const aside = document.querySelector('aside');
+const icon = document.querySelector('.icon');
+let asideProductos = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+    const fetchDato = async () => {
+        try{
+            const url = await fetch('api.json');
+            const dato = await url.json()
+            console.log(dato)
+        }
+        catch{
+            console.log('error')
+        }
+    }
+
+    fetchDato()
+})
