0a504b696581e2feab181ab88196d4be7e7947d6
gaboramirez <ignacio98ramirez@gmail.com>
2022-05-23 20:38:08 -0300

    cambiaso

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ document.addEventListener('click', (e) =>{
             toast: true,
             position: 'top-end',
             showConfirmButton: false,
-            timer: 3000,
+            timer: 1000,
             timerProgressBar: true,
             didOpen: (toast) => {
               toast.addEventListener('mouseenter', Swal.stopTimer)
