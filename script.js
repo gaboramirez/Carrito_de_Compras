1e3b926533eeeb01c68028b1100776e18765ae2f
gaboramirez <ignacio98ramirez@gmail.com>
2022-05-23 12:48:13 -0300

    agregar precio

--- script.js
+++ script.js
@@ -118,6 +118,8 @@ const agregarProductos = (productoCard) => {
     img.setAttribute('src', productoCard.img);
     const title = document.createElement('h3');
     title.innerHTML = productoCard.title;
+    const precio = document.createElement('h4');
+    precio.innerHTML = productoCard.precio;
     const iconSumar = document.createElement('i');
     iconSumar.className = 'fa-solid fa-circle-plus';
     const iconRestar = document.createElement('i');
